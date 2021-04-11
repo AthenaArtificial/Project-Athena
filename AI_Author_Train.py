1bf8819db60edb2ce3a3b553a6817c7c54d8b518
HanQLiu <hliu69@stevens.edu>
2021-04-11 17:22:10 -0400

    Example using Plato

--- AI_Author_Train.py
+++ AI_Author_Train.py
@@ -2,6 +2,8 @@
 ######  AthenaAI  ######
 ######    2021    ######
 ########################
+### Model Training #####
+########################
 
 import gpt_2_simple as gpt2
 import os
@@ -18,11 +20,11 @@ if not os.path.isdir(os.path.join("models", model_name)):
     print(f"Downloading {model_name} model...")
     gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
 
-author_path = "Authors/"
-file_name = "shakespeare.txt"  # Name of the book you want to train.
+author_path = "Authors/Plato/Processed/"
+file_name = "Plato.txt"  # Name of the book you want to train.
 file = author_path + file_name
 
 sess = gpt2.start_tf_sess()
-gpt2.finetune(sess, file, model_name=model_name, steps=1500)   # steps is max number of training steps
+gpt2.finetune(sess, file, model_name=model_name, steps=1000)   # steps is max number of training steps
 
 gpt2.generate(sess)
