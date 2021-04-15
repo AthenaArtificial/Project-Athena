15c95346e32b49a3c754c272a2700965d5809a8a
kavyajadav <63379421+kavyajadav@users.noreply.github.com>
2021-04-14 21:11:09 -0400

    Delete DigRem.py

--- DigRem.py
+++
@@ -1,7 +0,0 @@
-#opens the given file and eliminates for numbers
-with open('filename.txt') as f:
-    data = ''.join(i for i in f.read() if not i.isdigit())
-
-#opens another file to print the updated script
-with open('filename1.txt', 'w') as f:
-    f.write(data)
