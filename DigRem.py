3405340fdc107ab85601f640ca5b0b03389967e4
kavyajadav <63379421+kavyajadav@users.noreply.github.com>
2021-04-14 21:50:00 -0400

    Update DigRem.py

--- DigRem.py
+++ DigRem.py
@@ -1,7 +1,9 @@
 #opens the given file and eliminates for numbers
 with open('filename.txt') as f:
     data = ''.join(i for i in f.read() if not i.isdigit())
-
+    data = data.replace("-", "") #will replace the "-" with a null. Any symbol can be used instead of "-"
+    
+    
 #opens another file to print the updated script
 with open('filename1.txt', 'w') as f:
     f.write(data)
